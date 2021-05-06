bf9ef9ee5bba5df3ca0c7a5bc94a9e7ca997a821
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2021-05-06 20:14:22 +0530

    Create telepher.go

---
+++ telepher.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "net/http")
+const (
+  DefaultBaseURL = "https://api.telegram.org/bot"
+  DefaultBaseFileURL = "https://api.telegram.org/file/bot"
+  DefaultParseMode = "None"
+ )
+ 
+var DefaultClient = http.DefaultClient
+  package main
+
+import (
+  "net/http")
+const (
+  DefaultBaseURL = "https://api.telegram.org/bot"
+  DefaultBaseFileURL = "https://api.telegram.org/file/bot"
+  DefaultParseMode = "None"
+ )
+ 
+var DefaultClient = http.DefaultClient
+  
